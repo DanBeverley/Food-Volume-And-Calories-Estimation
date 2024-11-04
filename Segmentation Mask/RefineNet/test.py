ddd2bc35a09b099a4d9390b80e7ac0e6d61e3ba9
DanBeverley <161696810+DanBeverley@users.noreply.github.com>
2024-11-04 09:09:41 +0700

    Add files via upload

--- Segmentation Mask/RefineNet/test.py
+++ Segmentation Mask/RefineNet/test.py
@@ -32,3 +32,60 @@ def main(argv = None):
 
     input_image_shape = tf.shape(image_batch_tensor)
     image_height_width = input_image_shape[1:3]
+    image_height_width_float = tf.to_float(image_height_width)
+    image_height_width_multiple = tf.to_int32(tf.round(image_height_width_float/32)*32)
+
+    image_batch_tensor = tf.image.resize_images(image_batch_tensor, image_height_width_multiple)
+
+    global_step = tf.get_variables("global_step", [], initializer = tf.constant_initializer(0),
+                                   trainable = False)
+    logits = model.model(image_batch_tensor, is_training = False)
+    pred = tf.argmax(logits, dimension = 3)
+    pred = tf.expand_dims(pred, 3)
+    pred = tf.image.resize_nearest_neighbor(images = pred, size = image_height_width)
+    annotation_batch_tensor = tf.image.resize_nearest_neighbor(images = annotation_batch_tensor,
+                                                               size = image_height_width)
+    pred = tf.reshape(pred, [-1,])
+    gt = tf.reshape(annotation_batch_tensor, [-1,])
+    temp = tf.less_equal(gt, FLAGS.num_classes - 1)
+    weights = tf.cast(temp, tf.int32)
+    gt = tf.where(temp, gt, tf.cast(temp, tf.uint8))
+    acc, acc_update_op = tf.contrib.metrics.streaming_accuracy(pred, gt, weights=weights)
+    miou, miou_update_op = tf.contrib.metrics.streaming_mean_iou(pred, gt,
+                                                                 num_classes = FLAGS.num_classes,
+                                                                 weights = weights)
+    with tf.get_default_graph().as_default():
+        global_vars_init_op = tf.global_variables_initializer()
+        local_vars_init_op = tf.local_variables_initializer()
+        init = tf.group(local_vars_init_op, global_vars_init_op)
+
+        variable_averages = tf.train.ExponentialMovingAverage(.997, global_step)
+        saver = tf.train.Saver(variable_averages.variables_to_restore())
+
+        with tf.Session(config = tf.ConfigProto(allow_soft_placement = True)) as sess:
+            sess.run(init)
+            ckpt_state = tf.train.get_checkpoint_state(FLAGS.checkpoint_path)
+            model_path = os.path.join(FLAGS.checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
+            print("Restore from {}".format(model_path))
+            saver.restore(sess, model_path)
+
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord = coord)
+            for i in range(1449):
+                start = time.time()
+                image_np, annotation_np, pred_np, tmp_acc, tmp_miou = sess.run([image, annotation, pred,
+                                                                                acc_update_op,
+                                                                                miou_update_op])
+                _diff_time = time.time()-start
+                print("{}: cost {:.0f}ms").format(i, _diff_time * 1000)
+            acc_res = sess.run(acc)
+            miou_res = sess.run(miou)
+            print("Pascal VOC 2012 validation dataset pixel accuracy: " + str(acc_res))
+            print("Pascal VOC 2012 validation dataset Mean IoU: " + str(miou_res))
+    coord.request_stop()
+    coord.join(threads)
+if __name__ == '__main__':
+    tf.app.run()
+
+
+
