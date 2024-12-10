f4780c31255d978d12cd1ae96b19b1227161c242
DanBeverley <hoap43431@gmail.com>
2024-12-11 06:34:22 +0700

    update

--- Volume Estimation/net.py
+++ Volume Estimation/net.py
@@ -17,10 +17,10 @@ class model(nn.Module):
     def __init__(self, Encoder, num_features, block_channel):
         super(model, self).__init__()
 
-        self.E = Encoder
-        self.D = modules.D(num_features)
+        self.E   = Encoder
+        self.D   = modules.D(num_features)
         self.MFF = modules.MFF(block_channel)
-        self.R = modules.R(block_channel)
+        self.R   = modules.R(block_channel)
 
     def forward(self, x):
         x_block1, x_block2, x_block3, x_block4 = self.E(x)
